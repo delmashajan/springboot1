57bc491c3fa89a085a1319d1b261fa7ee286b057
delmashajan <delmashajan@gmail.com>
2022-12-08 15:40:23 +0530

    final

--- src/main/java/com/nest/studentappnew_backend/dao/StudentDao.java
+++ src/main/java/com/nest/studentappnew_backend/dao/StudentDao.java
@@ -1,6 +1,8 @@
 package com.nest.studentappnew_backend.dao;
 
 import com.nest.studentappnew_backend.model.Students;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,5 +13,10 @@ public interface StudentDao extends CrudRepository<Students,Integer> {
 
     @Query(value = "SELECT `id`, `admno`, `college`, `name`, `rollno` FROM `students` WHERE `name` LIKE %:name%",nativeQuery = true)
     List<Students> Searchstudent (@Param("name") String name);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM `students` WHERE `id`=:id",nativeQuery = true)
+    void deleteStudent(@Param("id") Integer id);
 
 }
