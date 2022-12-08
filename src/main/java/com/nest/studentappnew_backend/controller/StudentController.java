80fdfc945251f19d1be7146eb3ff6854c1f1c3e0
delmashajan <delmashajan@gmail.com>
2022-12-08 15:02:25 +0530

    add and search

--- src/main/java/com/nest/studentappnew_backend/controller/StudentController.java
+++ src/main/java/com/nest/studentappnew_backend/controller/StudentController.java
@@ -1,27 +1,44 @@
 package com.nest.studentappnew_backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.nest.studentappnew_backend.dao.StudentDao;
+import com.nest.studentappnew_backend.model.Students;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 
 public class StudentController {
 
-    @GetMapping("/")
+    @Autowired
+    private StudentDao dao;
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/add", consumes = "application/json",produces = "application/json")
+    public Map<String,String> Addstudent(@RequestBody Students s){
+        System.out.println(s.getName().toString() );
 
-    public String Homepage(){
-        return "Welcome to my website";
+        dao.save(s);
+        HashMap<String,String> map = new HashMap<>();
+        map.put("status","success");
+        return map;
     }
-    @GetMapping("/contact")
-    public String Contactpage(){
-        return "welcome to my contactpage";
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/view")
+    public List<Students> Viewstudent(){
+        return (List<Students>) dao.findAll();
     }
-    @GetMapping("/gallery")
-    public String Gallerypage(){
-        return "welcome to my gallery";
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/search",consumes = "application/json",produces = "application/json")
+    public String Searchstudent(@RequestBody Students s){
+        String name=String.valueOf(s.getName());
+        System.out.println(name);
+        return "hello";
+
     }
-    @GetMapping("/home")
-    public String Home(){
-        return "welcome to my homepage";
-    }
+
 }
